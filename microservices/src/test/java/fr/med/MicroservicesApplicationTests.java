57c0ba90b3fb8167e5e5b7115dd87ed59b69e6b2
medLille <mohamedayache13@gmail.com>
2020-03-31 17:11:57 +0200

    Creation de mon projet Spring Boot

---
+++ microservices/src/test/java/fr/med/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
